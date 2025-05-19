bd2585a48ab43d7834469856530b145e557d617c
shyamansi <66917662+shyamansi@users.noreply.github.com>
2025-05-19 12:03:43 +0530

    Second passes test case

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,8 @@
 class StringCalculator
     def self.add(nums)
      return 0 if nums.empty?
+    parts = nums.split(",").map(&:to_i)
+    parts.sum
     end
 
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -5,5 +5,8 @@ RSpec.describe StringCalculator do
     it "returns 0 for an empty string" do
       expect(StringCalculator.add("")).to eq(0)
     end
+    it "returns number for single input" do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
 end
 end
