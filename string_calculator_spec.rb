580e80151e47763672a92dd77a6cd84f5fab91de
shyamansi <66917662+shyamansi@users.noreply.github.com>
2025-05-19 12:12:57 +0530

    Third test case passed

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -8,5 +8,8 @@ RSpec.describe StringCalculator do
     it "returns number for single input" do
       expect(StringCalculator.add("1")).to eq(1)
     end
+    it "returns the sum of two numbers separated by a comma" do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
 end
 end
